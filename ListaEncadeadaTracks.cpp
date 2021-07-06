3b31ca4fa5c85b07f26411fcf1abc66cb4e107aa
LucasCastro27 <84679403+LucasCastro27@users.noreply.github.com>
2021-07-06 09:27:37 -0300

    implementação das funções

--- ListaEncadeadaTracks.cpp
+++ ListaEncadeadaTracks.cpp
@@ -296,6 +296,133 @@ void ListaEncadeadaTracks::importaBin(int tam) {
     //Apos a criacao do vetor ser terminada, chama a funcao de impressao do vetor
     imprimeTestes(vetor);
 }
+void ListaEncadeadaTracks::NovaLista(string arq2)
+{
+    ifstream tracks;
+    string nameT,popularityT,duration_ms,explicito,danceability,energy,key,loudness,idT,l,
+    mode,speechiness,acousticness,instrumentalness,liveness,valence,tempo,time_signature,artistsT,id_artists,release_date;
 
+
+    bool explicito_bool;
+
+    int popularityT_int;
+    int duration_ms_int;
+    int time_signature_int;
+
+    float danceability_float;
+    float energy_float;
+    float key_float;
+    float loudness_float;
+    float mode_float;
+    float speechiness_float;
+    float acousticness_float;
+    float instrumentalness_float;
+    float liveness_float;
+    float valence_float;
+    float tempo_float;
+
+    tracks.open(arq2, ios::in | ios::out);
+
+    if  (tracks.fail()) {
+        cout << "> Erro na leitura de tracks.csv" << endl;
+        exit(1);
+    }
+
+    ///Descarta a primeira linha que nao contem informacoes
+    getline(tracks,l,'\n');
+
+    ///Loop que se repete ate que todo o arquivo tenha sido percorrido e armazena as informacoes na lista
+     while(tracks.good()) {
+        getline(tracks,idT,',');
+
+        getline(tracks,nameT,',');
+
+        getline(tracks,popularityT,',');
+        stringstream pop(popularityT);
+        pop  >> popularityT_int;
+
+        getline(tracks,duration_ms,',');
+        stringstream du(duration_ms);
+        du >> duration_ms_int;
+
+        getline(tracks,explicito,',');
+        stringstream ex(explicito);
+        ex >> explicito_bool;
+
+        getline(tracks,l,'[');
+        getline(tracks,artistsT,']');
+        getline(tracks,l,',');
+
+
+        getline(tracks,l,'[');
+        getline(tracks,id_artists,']');
+        getline(tracks,l,',');
+
+        getline(tracks, release_date, ',');
+
+        getline(tracks,danceability , ',');
+        stringstream da(danceability);
+        da >> danceability_float;
+
+        getline(tracks,energy , ',');
+        stringstream en(energy);
+        en >> energy_float;
+
+        getline(tracks,key , ',');
+        stringstream ke(key);
+        ke >> key_float;
+
+        getline(tracks,loudness , ',');
+        stringstream lo(loudness);
+        lo >> loudness_float;
+
+        getline(tracks,mode , ',');
+        stringstream mo(mode);
+        mo >> mode_float;
+
+        getline(tracks,speechiness , ',');
+        stringstream sp(speechiness);
+        sp >> speechiness_float;
+
+        getline(tracks,acousticness , ',');
+        stringstream ac(acousticness);
+        ac >> acousticness_float;
+
+        getline(tracks,instrumentalness , ',');
+        stringstream in(instrumentalness);
+        in >> instrumentalness_float;
+
+        getline(tracks,liveness, ',');
+        stringstream li(liveness);
+        li >> liveness_float;
+
+        getline(tracks,valence, ',');
+        stringstream va(valence);
+        va >> valence_float;
+
+        getline(tracks,tempo, ',');
+        stringstream te(tempo);
+        te >> tempo_float;
+
+        getline(tracks,time_signature , '\n');
+        stringstream ti(time_signature);
+        ti >> time_signature_int;
+
+        NovaTrack(idT,nameT,popularityT_int,duration_ms_int,explicito_bool,artistsT,id_artists,release_date,danceability_float,
+                            energy_float,key_float,loudness_float,mode_float,speechiness_float,acousticness_float,instrumentalness_float,
+                            liveness_float,valence_float,tempo_float,time_signature_int);
+        }
+
+        tracks.close();
+        deletaultimo();
+}
+void ListaEncadeadaTracks::deletaultimo()
+{
+    NoT *p=ultimo->getAnt();
+    delete ultimo;
+    ultimo=p;
+    ultimo->setProx(NULL);
+    n--;
+}
 
 
