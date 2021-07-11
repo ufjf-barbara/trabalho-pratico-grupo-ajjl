585dcfbc0e6e8acf3a8c8bcc9a0995f1c041da66
andrediasn <andredias@ice.ufjf.br>
2021-07-11 18:29:21 -0300

    att

--- MetodosOrdenacao.cpp
+++ MetodosOrdenacao.cpp
@@ -64,15 +64,15 @@ void MetodosOrdenacao::ordenaQuick() {
 }
 
 void MetodosOrdenacao::ordenaMerge() {
-    // pega tam de input
+    BlocoMerge();
 
-    // chama Merge
+    
 }
 
 void MetodosOrdenacao::ordenaHeap() {
-    // pega tam de input
+    BlocoHeap();
 
-    // chama Heap
+    
 }
 
 void MetodosOrdenacao::ModuloTeste() {
@@ -251,19 +251,15 @@ void MetodosOrdenacao::testedeinput()
 }
 
 // Contadores
-void MetodosOrdenacao::zerarmetricas(int metrica[])
-{
+void MetodosOrdenacao::zerarmetricas(int metrica[]) {
     metrica[0]=0;
     metrica[1]=0;
     metrica[2]=0;
 }
-void MetodosOrdenacao::BlocoMerge()
-{
+void MetodosOrdenacao::BlocoMerge() {
     int MetricasMerge[3][5][3];
-    for (int k=0;k<3;k++)
-    {
-        for(int j=0;j<5;j++)
-        {
+    for (int k=0;k<3;k++) {
+        for(int j=0;j<5;j++) {
             int metricas[3];
             zerarmetricas(metricas);
             mergesortinicio(valores[j],metricas);
@@ -273,13 +269,10 @@ void MetodosOrdenacao::BlocoMerge()
         }
     } 
 }
-
 void MetodosOrdenacao::BlocoQuick() {   
     int MetricasQuick[3][5][3];
-    for (int k=0;k<3;k++)
-    {
-        for(int j=0;j<5;j++)
-        {
+    for (int k=0;k<3;k++) {
+        for(int j=0;j<5;j++) {
             int metricas[3];
             zerarmetricas(metricas);
             vector<Artista> vetor = artistasaleatorios(valores[j]);
@@ -290,17 +283,14 @@ void MetodosOrdenacao::BlocoQuick() {
         }
     } 
 }
-
 void MetodosOrdenacao::BlocoHeap() {   
     int MetricasHeap[3][5][3];
-    for (int k=0;k<3;k++)
-    {
-        for(int j=0;j<5;j++)
-        {
+    for (int k=0;k<3;k++) {
+        for(int j=0;j<5;j++) {
             int metricas[3];
             zerarmetricas(metricas);
             vector<Artista> vetor = artistasaleatorios(valores[j]);
-            heapSort(&vetor, 0, valores[j-1], metricas);
+            heapSort(&vetor, valores[j], metricas);
             MetricasHeap[k][j][0]=metricas[0];
             MetricasHeap[k][j][1]=metricas[1];
             MetricasHeap[k][j][2]=metricas[2];
