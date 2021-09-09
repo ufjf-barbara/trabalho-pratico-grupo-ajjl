3675cb52dbdb5514dd57f6331d074830558c07be
julianapsilva <julianadepaula001@gmail.com>
2021-09-08 22:21:25 -0300

    mudando menu

--- src/etapa4/main.cpp
+++ src/etapa4/main.cpp
@@ -13,50 +13,19 @@ Comandos para compilar via terminal:
     g++ *.c* -o main
     ./main SEU/DIRETORIO/trabalho-pratico-grupo-ajjl
 */
-void submenuCasamento(){ 
-     int opcao;
-    do{
-        cout << "Executar qual etapa?" << endl;
-        cout << " 1 - Forca bruta" << endl;
-        cout << " 2 - KMP" << endl;
-        cout << " 3 - BHM" << endl;
-        cout << " 4 - Voltar" << endl;
-        cin >> opcao;
-        if (opcao == 1) {
-            cout << "Executando método Forca Bruta..." << endl;
 
-            cout << "Etapa concluida!" << endl;
-        }
-        else if (opcao == 2) {
-            cout << " Executando método KMP..." << endl;
-
-            cout << "Etapa concluida!" << endl;
-        }
-        else if (opcao == 3) {
-            cout << " Executando método BHM..." << endl;
-
-            cout << "Etapa concluida!" << endl;
-        }
-    } while (opcao != 4);
+string nomeSequencia(int indice){ 
+        string tes = "dna";
+        tes += (indice+1) + '0';
+        tes += ".txt";
+        return tes;
 }
 
-void menu() { // Menu para selecionar as opcoes 
-    int opcao;
-    do{
-        cout << "Executar qual etapa?" << endl;
-        cout << " 1 - Casamento de padrao" << endl;
-        cout << " 2 - Metodo de Compressao" << endl;
-        cout << " 3 - Sair" << endl;
-        cin >> opcao;
-        if (opcao == 1) {
-            submenuCasamento();
-        }
-        else if (opcao == 2) {
-            cout<< "chamadas compressao" << endl;
-            cout << " Etapa concluida" << endl;
-        }
-    }
-    while(opcao != 3); // Sair
+string nomePadrao(int indice){ 
+        string tes = "padrao";
+        tes += (indice+1) + '0';
+        tes += ".txt";
+        return tes;
 }
 
 string leArquivoTexto(string arquivo)
@@ -78,6 +47,78 @@ string leArquivoTexto(string arquivo)
 
     return aux;
 }
+
+void submenuCasamento(int qtSeq, int qtPadrao){ 
+     int opcao;
+     CasamentoPadrao padrao;
+    do{
+        cout << "Executar qual etapa?" << endl;
+        cout << " 1 - Forca bruta" << endl;
+        cout << " 2 - KMP" << endl;
+        cout << " 3 - BHM" << endl;
+        cout << " 4 - Voltar" << endl;
+        cin >> opcao;
+        if (opcao == 1) {
+            cout << "Executando método Forca Bruta..." << endl;
+            for (int i = 0; i < qtSeq; i++){
+                string sequenciaT = leArquivoTexto(nomeSequencia(i));
+                for (int j = 0; j < qtPadrao; j++)
+                {
+                    string padraoP = leArquivoTexto(nomePadrao(j));
+                    padrao.ForcaBruta(sequenciaT, padraoP);
+                }
+            }
+                cout << "Etapa concluida!" << endl;
+        }
+        else if (opcao == 2) {
+            cout << " Executando método KMP..." << endl;
+            for (int i = 0; i < qtSeq; i++){
+                string sequenciaT = leArquivoTexto(nomeSequencia(i));
+                for (int j = 0; j < qtPadrao; j++)
+                {
+                    string padraoP = leArquivoTexto(nomePadrao(j));
+                    padrao.KMP(sequenciaT, padraoP);
+                }
+            }
+
+            cout << "Etapa concluida!" << endl;
+        }
+        else if (opcao == 3) {
+            cout << " Executando método BHM..." << endl;
+            for (int i = 0; i < qtSeq; i++){
+                string sequenciaT = leArquivoTexto(nomeSequencia(i));
+                for (int j = 0; j < qtPadrao; j++)
+                {
+                    string padraoP = leArquivoTexto(nomePadrao(j));
+                    padrao.BHMsearch(sequenciaT, padraoP);
+                }
+            }
+
+            cout << "Etapa concluida!" << endl;
+        }
+    } while (opcao != 4);
+}
+
+void menu(int qtSeq, int qtPadrao) { // Menu para selecionar as opcoes 
+    int opcao;
+    do{
+        cout << "Executar qual etapa?" << endl;
+        cout << " 1 - Casamento de padrao" << endl;
+        cout << " 2 - Metodo de Compressao" << endl;
+        cout << " 3 - Sair" << endl;
+        cin >> opcao;
+        if (opcao == 1) {
+            submenuCasamento(qtSeq, qtPadrao);
+        }
+        else if (opcao == 2) {
+            cout<< "chamadas compressao" << endl;
+            cout << " Etapa concluida" << endl;
+        }
+    }
+    while(opcao != 3); // Sair
+}
+
+
 
 /* void leArquivoPadrao(string arquivo, string *sequenciaP)
 {
@@ -109,33 +150,26 @@ int main() // ./main <caminhoSeq> <qntArqSeq> <caminhoPad> <qntArqPad>
     //string qtPStr = argv[4];
     //int qtP = stoi(qtPStr);
     
-    string arquivo= "dna1.txt";
-    string sequenciaT = leArquivoTexto(arquivo);
+    // string arquivo= "dna1.txt";
+    // string sequenciaT = leArquivoTexto(arquivo);
     // menu();
-    // cout << sequenciaT << endl;
 
-    arquivo= "padrao.txt";
-    string padraoP = leArquivoTexto(arquivo);
-    CasamentoPadrao padrao;
-    cout << "Forca Bruta" << endl;
-    padrao.ForcaBruta(sequenciaT, "tttac");
-    cout << "BHM" << endl;
-    padrao.BHMsearch(sequenciaT, "tttac");
-    cout << "KMP" << endl;
-    padrao.KMP(sequenciaT, "tttac");
-
-    string tes = "padrao";
-    tes += 1 + '0';
-    tes += ".txt";
-    cout << "STRING " << tes << endl;
-
-    // auto s = std::format("padrao{}", 1); 
-    // cout << "res " << s;
-    // string test = string.Format("padrao{}", 1);
-    //   std::cout << fmt::format("padrao{}", 1);
-
-    // cout << "res " << test;
-
+   // arquivo= "padrao.txt";
+    // string padraoP = leArquivoTexto(arquivo);
+    // CasamentoPadrao padrao;
+    // cout << "Forca Bruta" << endl;
+    // padrao.ForcaBruta(sequenciaT, "tttac");
+    // cout << "BHM" << endl;
+    // padrao.BHMsearch(sequenciaT, "tttac");
+    // cout << "KMP" << endl;
+    // padrao.KMP(sequenciaT, "tttac");
+    int qtPadrao;
+    int qtSeq;
+    cout << "Digite o numero de sequencias a serem lidas: ";
+    cin >> qtSeq;
+    cout << "Digite o numero de padroes a serem lidos: ";
+    cin >> qtPadrao;
+    menu(qtSeq, qtPadrao);
 
     return 0;
 }
