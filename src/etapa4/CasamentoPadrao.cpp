f18392146061d34f9379070fb186709f5c735f1e
julianapsilva <julianadepaula001@gmail.com>
2021-09-08 21:25:39 -0300

    organiza os algoritmos em um TAD

---
+++ src/etapa4/CasamentoPadrao.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include "CasamentoPadrao.h"
+
+using namespace std;
+
+void CasamentoPadrao::ForcaBruta(string T, string P){
+    int tamT = T.length();
+    int tamP = P.length();
+    int ocorrencias = 0;
+    for(int i = 0; i <= (tamT-tamP); i++){
+        int k = i;
+        int j = 0;
+        while(T[k] == P[j]){
+            j ++;
+            k ++;
+            if(j==tamP){
+                //cout << "Casamento na posicao: " << i << endl;
+                ocorrencias ++;
+                break;
+            }
+        }
+    }
+    cout << "Numero total de ocorrencias ----> " << ocorrencias << endl;
+}
+
+void prefixo(string P, int tamP, int* lps){
+	lps[0] = 0; 
+	int j = 0;
+	int i = 1;
+	while (i < tamP) {
+		if (P[i] == P[j]) {
+			j++;
+			lps[i] = j;
+			i++;
+		}
+		else {
+			if (j != 0) 
+				j = lps[j - 1];
+			else {
+				lps[i] = 0;
+				i++;
+			}
+		}
+	}
+}
+
+void CasamentoPadrao::KMP(string T, string P){
+	int tamT = T.length();
+    int tamP = P.length();
+	int* lps = new int [tamP];
+    int ocorrencias = 0;
+
+	prefixo(P, tamP, lps);
+
+	int i = 0; 
+	int j = 0;
+	while (i < tamT) {
+		if (P[j] == T[i]) {
+			j++;
+			i++;
+		}
+		if (j == tamP) {
+			//cout << "Casamento na posicao: " << i-j << endl;
+            ocorrencias ++;
+			j = lps[j-1];
+		}
+		else if (i < tamT && P[j] != T[i]) {
+			if (j != 0)
+				j = lps[j - 1];
+			else
+				i = i + 1;
+		}
+	}
+    cout << "Numero total de ocorrencias ----> " << ocorrencias << endl;
+}
+
+void CasamentoPadrao::BHMsearch(string sequenciaT, string padraoP){
+    int sequenciaSize = sequenciaT.length();
+    int padraoSize = padraoP.length();
+    int d[256], j, k;
+    int ocorrencias = 0;
+    // inicializacao da tabela de deslocamentos
+    for(int j=0; j<= 256; j++)
+        d[j] = padraoSize;
+    for(int j=1; j< padraoSize;j++)
+        d[padraoP[j - 1]] = padraoSize - j;
+
+    // fase de pesquisa
+    for (int i = padraoSize; i <= sequenciaSize; i += d[sequenciaT[i - 1]])
+    {
+        for(j = padraoSize, k = i; sequenciaT[k-1] == padraoP[j-1] && j>0; j--)
+            k--;
+        if(j == 0){
+            ocorrencias++;
+            //cout << "Casamento na posicao: " << k << endl;
+        }
+    }
+    cout << "Numero total de ocorrencias ----> " << ocorrencias << endl;
+}
--- src/etapa4/main.cpp
+++ src/etapa4/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <sstream>
 #include <fstream>
+#include "CasamentoPadrao.h"
 
 
 using namespace std;
@@ -99,102 +100,6 @@ string leArquivoTexto(string arquivo)
     return aux;
 } */
 
-void ForcaBruta(string T, string P){
-    int tamT = T.length();
-    int tamP = P.length();
-    int ocorrencias = 0;
-    for(int i = 0; i <= (tamT-tamP); i++){
-        int k = i;
-        int j = 0;
-        while(T[k] == P[j]){
-            j ++;
-            k ++;
-            if(j==tamP){
-                //cout << "Casamento na posicao: " << i << endl;
-                ocorrencias ++;
-                break;
-            }
-        }
-    }
-    cout << "Numero total de ocorrencias ----> " << ocorrencias << endl;
-}
-
-void BHMsearch(string sequenciaT, string padraoP){
-    int sequenciaSize = sequenciaT.length();
-    int padraoSize = padraoP.length();
-    int d[256], j, k;
-    int ocorrencias = 0;
-    // inicializacao da tabela de deslocamentos
-    for(int j=0; j<= 256; j++)
-        d[j] = padraoSize;
-    for(int j=1; j< padraoSize;j++)
-        d[padraoP[j - 1]] = padraoSize - j;
-
-    // fase de pesquisa
-    for (int i = padraoSize; i <= sequenciaSize; i += d[sequenciaT[i - 1]])
-    {
-        for(j = padraoSize, k = i; sequenciaT[k-1] == padraoP[j-1] && j>0; j--)
-            k--;
-        if(j == 0){
-            ocorrencias++;
-            //cout << "Casamento na posicao: " << k << endl;
-        }
-    }
-    cout << "Numero total de ocorrencias ----> " << ocorrencias << endl;
-}
-
-void prefixo(string P, int tamP, int* lps){
-	lps[0] = 0; 
-	int j = 0;
-	int i = 1;
-	while (i < tamP) {
-		if (P[i] == P[j]) {
-			j++;
-			lps[i] = j;
-			i++;
-		}
-		else {
-			if (j != 0) 
-				j = lps[j - 1];
-			else {
-				lps[i] = 0;
-				i++;
-			}
-		}
-	}
-}
-
-void KMP(string T, string P){
-	int tamT = T.length();
-    int tamP = P.length();
-	int* lps = new int [tamP];
-    int ocorrencias = 0;
-
-	prefixo(P, tamP, lps);
-
-	int i = 0; 
-	int j = 0;
-	while (i < tamT) {
-		if (P[j] == T[i]) {
-			j++;
-			i++;
-		}
-		if (j == tamP) {
-			//cout << "Casamento na posicao: " << i-j << endl;
-            ocorrencias ++;
-			j = lps[j-1];
-		}
-		else if (i < tamT && P[j] != T[i]) {
-			if (j != 0)
-				j = lps[j - 1];
-			else
-				i = i + 1;
-		}
-	}
-    cout << "Numero total de ocorrencias ----> " << ocorrencias << endl;
-}
-
-
 int main() // ./main <caminhoSeq> <qntArqSeq> <caminhoPad> <qntArqPad>
 {
     //string caminhoSeq = argv[1]; 
@@ -211,12 +116,13 @@ int main() // ./main <caminhoSeq> <qntArqSeq> <caminhoPad> <qntArqPad>
 
     arquivo= "padrao.txt";
     string padraoP = leArquivoTexto(arquivo);
+    CasamentoPadrao padrao;
     cout << "Forca Bruta" << endl;
-    ForcaBruta(sequenciaT, "tttac");
+    padrao.ForcaBruta(sequenciaT, "tttac");
     cout << "BHM" << endl;
-    BHMsearch(sequenciaT, "tttac");
+    padrao.BHMsearch(sequenciaT, "tttac");
     cout << "KMP" << endl;
-    KMP(sequenciaT, "tttac");
+    padrao.KMP(sequenciaT, "tttac");
 
     string tes = "padrao";
     tes += 1 + '0';
